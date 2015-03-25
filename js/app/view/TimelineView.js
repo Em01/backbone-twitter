5dca0436d4e3ac7b377fc4010c2a1b4800a233a4
Em01 <emma_williams1@hotmail.co.uk>
2015-03-25 14:26:53 +0000

    timeline view js

---
+++ js/app/view/TimelineView.js
@@ -0,0 +1,15 @@
+var com = || {};
+com.apress = com.apress || {};
+com.apress.view = com.apress.view ||({
+
+	el: '#timeline',
+
+	template: Handlebars.compile($("#timeline.template").html()),
+
+	initialize: function(options){
+	},
+	render: function(){
+	},
+
+});
+	

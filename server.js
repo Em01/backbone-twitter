a4e70a9338dbf61a4557b2d53da7bdf4bb4de101
Em01 <emma_williams1@hotmail.co.uk>
2015-03-25 13:40:57 +0000

    more work on the server without any handlers added for client GET and POST methods

--- server.js
+++ server.js
@@ -22,3 +22,25 @@ function connectToTwitter() {
 connectToTwitter();
 
 //additional setup to allow CORS requests
+var allowCrossDomain = function(req, response, next) {
+	response.header('Access-Control-Allow-Origin', "http://localhost");
+	response.header('Access-Control-Allow-Methods', 'OPTIONS, GET,PUT,POST,DELETE');
+	response.header('Access-Control-Allow-Headers', 'Content-Type');
+
+	if ('OPTIONS' == req.method) {
+		response.send(200);
+	}
+	else {
+		next();
+	}
+};
+
+app.configure(function() {
+	app.use(allowCrossDomain);
+	//Parses the JSON object given in the body request
+
+app.use(express.bodyParser());
+});
+
+//start up the app on port 8080
+app.listen(8080);
